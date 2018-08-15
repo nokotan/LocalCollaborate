68c2f054cb9f115b64b38868e253d81309b99e3a
aaa <bbb>
2018-08-16 01:48:42 +0900

    ggg

--- Assets/Editor/LocalCollaborate.cs
+++ Assets/Editor/LocalCollaborate.cs
@@ -1,13 +1,89 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using LibGit2Sharp;
 
 public class LocalCollaborate : EditorWindow {
 
-    [SerializeField]
-    string RemotePath;
+    class SettingTab
+    {
+        public string RemotePath { get; set; }
+        public string UserName { get; set; }
+        public string EMailAddress { get; set; }
+        public Action<string, string> OnUserDataChanged { get; set; }
+
+        void OnGUIInternal(string name, Func<string> updateFunction)
+        {
+            EditorGUILayout.LabelField(name);
+
+            EditorGUI.BeginChangeCheck();
+            var userData = updateFunction();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                OnUserDataChanged.Invoke(name, userData);
+            }
+        }
+
+        public void OnGUI()
+        {
+            OnGUIInternal("RemotePath",     () => RemotePath = EditorGUILayout.TextField(RemotePath));
+            OnGUIInternal("UserName",       () => UserName = EditorGUILayout.TextField(UserName));
+            OnGUIInternal("EMailAddress",   () => EMailAddress = EditorGUILayout.TextField(EMailAddress));
+        }
+    }
+
+    struct CommitBaseData
+    {
+        public string Id { get; set; }
+        public string Auther { get; set; }
+        public string Message { get; set; }
+    }
+
+    List<CommitBaseData> CommitData;
+
+    void UpdateCommitData()
+    {
+        var masterBranch = LocalRepository.Branches["master"];
+
+        CommitData = masterBranch.Commits.Select(
+            commit => new CommitBaseData()
+            {
+                Id = commit.Id.ToString().Substring(0, 8),
+                Auther = commit.Author.Name,
+                Message = commit.Message
+            })
+            .ToList();
+
+        Debug.Log("Commit Data Updated");
+    }
+
+    struct FileStatusBaseData
+    {
+        public FileStatus status { get; set; }
+        public string filePath { get; set; }
+    }
+
+    List<FileStatusBaseData> FileStatusData;
+
+    void UpdateFileStatus()
+    {
+        FileStatusData = LocalRepository.RetrieveStatus()
+            .Where(item => item.State != FileStatus.Ignored)
+            .Select(
+                item => new FileStatusBaseData()
+                {
+                    status = item.State,
+                    filePath = item.FilePath
+                })
+            .ToList();
+
+        Debug.Log("File Status Updated");
+
+    }
+
     Repository LocalRepository;
 
     [MenuItem("Window/LocalCollaborate/Open")]
@@ -22,90 +98,129 @@ public class LocalCollaborate : EditorWindow {
     Vector2 ScrollRectCommit;
     string CommitMessage;
 
-    [SerializeField]
-    string UserName;
-    [SerializeField]
-    string EMail;
-
     string StatusString = "";
 
-    void OnGUI()
+    SettingTab settingTab;
+
+    void InitializeSettingTab()
     {
-        if (LocalRepository == null)
+        settingTab = new SettingTab()
         {
-            try
-            {
-                LocalRepository = new Repository(".");
-                RemotePath = LocalRepository.Network.Remotes["origin"].Url;
-            }
-            catch (RepositoryNotFoundException e)
-            {
-                EditorGUILayout.LabelField("This project is not git repository.");
-                Debug.Log(e.Message);
-                return;
-            }
-        }
-
-        Selected = GUILayout.Toolbar(Selected, new string[] { "Setting", "Branches", "Commits" });
-
-        if (Selected == 0)
+            UserName = LocalCollaborateUserData.UserName,
+            EMailAddress = LocalCollaborateUserData.EMailAddress
+        };
+        
+        // RemotePath Changed Handler
+        settingTab.OnUserDataChanged += (dataName, url) =>
         {
-            EditorGUILayout.LabelField("Remote Path");
-
-            EditorGUI.BeginChangeCheck();
-            RemotePath = EditorGUILayout.TextField(RemotePath);
-
-            Remote remote = LocalRepository.Network.Remotes["origin"];
-
-            if (EditorGUI.EndChangeCheck())
+            if (dataName == "RemotePath")
             {
+                var remoteList = LocalRepository.Network.Remotes;
+                var remote = remoteList["origin"];
 
                 if (remote == null)
                 {
-                    remote = LocalRepository.Network.Remotes.Add("origin", RemotePath);
+                    remote = remoteList.Add("origin", url);
                 }
                 else
                 {
-                    LocalRepository.Network.Remotes.Update(remote, r => r.Url = RemotePath);
+                    remoteList.Update(remote, r => r.Url = url);
                 }
-               
-                LocalRepository.Branches.Update(LocalRepository.Head,
+
+                var masterBranch = LocalRepository.Branches["master"];
+
+                LocalRepository.Branches.Update(masterBranch,
                     b => b.Remote = remote.Name,
-                    b => b.UpstreamBranch = LocalRepository.Head.CanonicalName
+                    b => b.UpstreamBranch = masterBranch.CanonicalName
                 );
             }
+        };
 
-            if (GUILayout.Button("Push"))
+        // UserName Changed Handler
+        settingTab.OnUserDataChanged += (dataName, userName) =>
+        {
+            if (dataName == "UserName")
             {
-                LocalRepository.Network.Push(LocalRepository.Network.Remotes["origin"], LocalRepository.Head.CanonicalName);
-                StatusString = "Push Finished";
+                LocalCollaborateUserData.UserName = userName;
+            }
+        };
+
+        // EMailAddress Changed Handler
+        settingTab.OnUserDataChanged += (dataName, eMailAddress) =>
+        {
+            if (dataName == "EMailAddress")
+            {
+                LocalCollaborateUserData.EMailAddress = eMailAddress;
+            }
+        };
+    }
+
+    bool InitializeRepository()
+    {
+        if (Repository.IsValid("."))
+        {
+            LocalRepository = new Repository(".");
+
+            var remote = LocalRepository.Network.Remotes["origin"];
+
+            if (remote != null)
+            {
+                settingTab.RemotePath = remote.Url;
             }
 
-            if (GUILayout.Button("Fetch"))
-            {
-                LocalRepository.Network.Fetch(remote);
-                StatusString = LocalRepository.Head.CanonicalName;
-            }
+            UpdateCommitData();
+            UpdateFileStatus();
+            
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
-            if (GUILayout.Button("Merge"))
-            {
-                var Sign = new Signature(new Identity(UserName, EMail), System.DateTime.Now);
-                var Result = LocalRepository.Merge(LocalRepository.Head.TrackedBranch, Sign);
-                
-                if (Result.Status == MergeStatus.Conflicts)
-                {
-                    Debug.Log("Conflict!");
-                }
-               
-                AssetDatabase.Refresh();
-                StatusString = "Merge Finished";
-            }
+    void Awake()
+    {
+        Debug.Log("Awaked");
+        
+        InitializeSettingTab();
+        InitializeRepository();
 
-            EditorGUILayout.LabelField("Name");
-            UserName = EditorGUILayout.TextField(UserName);
+        LastFetchedTime = DateTime.Now;
+        CommitData = new List<CommitBaseData>();
+    }
 
-            EditorGUILayout.LabelField("EMail");
-            EMail = EditorGUILayout.TextField(EMail);
+    DateTime LastFetchedTime;
+
+    void Fetch()
+    {
+        var remote = LocalRepository.Network.Remotes["origin"];
+        LocalRepository.Network.Fetch(remote);
+
+        Debug.Log("Fetched");
+    }
+
+    void Merge()
+    {
+        var Sign = LocalCollaborateUserData.GetSignature();
+        var Result = LocalRepository.Merge(LocalRepository.Head.TrackedBranch, Sign);
+
+        if (Result.Status == MergeStatus.Conflicts)
+        {
+            Debug.Log("Conflict!");
+        }
+
+        AssetDatabase.Refresh();
+        StatusString = "Merge Finished";
+
+        UpdateCommitData();
+    }
+
+    void OnInspectorUpdate()
+    {
+        if (DateTime.Now - LastFetchedTime > TimeSpan.FromSeconds(2.0))
+        {
+            Fetch();
 
             var trackingBranch = LocalRepository.Head.TrackedBranch;
             var log = LocalRepository.Commits.QueryBy(new CommitFilter
@@ -115,65 +230,127 @@ public class LocalCollaborate : EditorWindow {
             {
                 StatusString = "There is remote changes!";
             }
+
+            LastFetchedTime = DateTime.Now;
+        }
+    }
+
+    void OnGUIInSynclonizeTab()
+    {
+        if (GUILayout.Button("Push"))
+        {
+            LocalRepository.Network.Push(LocalRepository.Network.Remotes["origin"], LocalRepository.Head.CanonicalName);
+            StatusString = "Push Finished";
+        }
+
+        if (GUILayout.Button("Fetch"))
+        {
+            Fetch();
+        }
+
+        if (GUILayout.Button("Merge"))
+        {
+            Merge();
+        }
+    }
+
+    void OnGUIInCommits()
+    {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Commits");
+
+        if (GUILayout.Button("Refresh"))
+        {
+            UpdateCommitData();
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        using (new EditorGUI.IndentLevelScope())
+        {
+            ScrollRectCommit = EditorGUILayout.BeginScrollView(ScrollRectCommit, GUI.skin.box);
+
+            foreach (var commit in CommitData)
+            {
+                EditorGUILayout.LabelField(commit.Id, commit.Message);
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Status");
+
+        if (GUILayout.Button("Refresh"))
+        {
+            UpdateFileStatus();
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        using (new EditorGUI.IndentLevelScope())
+        {
+            ScrollRect = EditorGUILayout.BeginScrollView(ScrollRect, GUI.skin.box);
+
+            foreach (var item in FileStatusData) // .Where(obj => obj.State != FileStatus.NewInWorkdir))
+            {
+                EditorGUILayout.LabelField(item.status.ToString(), item.filePath);
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        CommitMessage = EditorGUILayout.TextArea(CommitMessage);
+
+        if (GUILayout.Button("Commit"))
+        {
+            var Sign = LocalCollaborateUserData.GetSignature();
+            LocalRepository.Stage("*");
+            LocalRepository.Commit(CommitMessage, Sign, Sign);
+
+            CommitMessage = "";
+            StatusString = "Commit Finished!";
+
+            UpdateCommitData();
+        }
+    }
+
+    void OnGUI()
+    {
+        if (LocalRepository == null && InitializeRepository())
+        {
+            EditorGUILayout.LabelField("Local Collaborate is not active on this project.");
+
+            if (GUILayout.Button("Start Local Collaborate"))
+            {
+                Repository.Init(".");
+            }
+
+            return;
+        }
+
+        Selected = GUILayout.Toolbar(Selected, new string[] { "Commits", "Synclonize", "Setting" });
+
+        if (Selected == 2)
+        {
+            settingTab.OnGUI();
         }
 
         if (Selected == 1)
         {
-            EditorGUILayout.LabelField("Branches");
-
-            using (new EditorGUI.IndentLevelScope())
-            {
-                foreach (var branch in LocalRepository.Branches)
-                {
-                    EditorGUILayout.LabelField(branch.FriendlyName);
-                }
-            }
+            OnGUIInSynclonizeTab();
         }
 
-        if (Selected == 2)
+        if (Selected == 0)
         {
-            EditorGUILayout.LabelField("Commits");
-
-            using (new EditorGUI.IndentLevelScope())
-            {
-                ScrollRectCommit = EditorGUILayout.BeginScrollView(ScrollRectCommit);
-
-                foreach (var commit in LocalRepository.Head.Commits)
-                {
-                    EditorGUILayout.LabelField(commit.Id.ToString().Substring(0, 8), commit.Message);
-                }
-
-                EditorGUILayout.EndScrollView();
-            }
-
-            EditorGUILayout.LabelField("Status");
-
-            using (new EditorGUI.IndentLevelScope())
-            {
-                ScrollRect = EditorGUILayout.BeginScrollView(ScrollRect);
-
-                foreach (var item in LocalRepository.RetrieveStatus()) // .Where(obj => obj.State != FileStatus.NewInWorkdir))
-                {
-                    EditorGUILayout.LabelField(item.State.ToString(), item.FilePath);
-                }
-
-                EditorGUILayout.EndScrollView();
-            }
-
-            CommitMessage = EditorGUILayout.TextArea(CommitMessage);
-
-            if (GUILayout.Button("Commit"))
-            {
-                var Sign = new Signature(new Identity(UserName, EMail), System.DateTime.Now);
-                LocalRepository.Stage("*");
-                LocalRepository.Commit(CommitMessage, Sign, Sign);
-
-                CommitMessage = "";
-                StatusString = "Commit Finished!";
-            }
+            OnGUIInCommits();
         }
-
 
         EditorGUILayout.LabelField(StatusString);
+    }
+
+    void OnProjectChange()
+    {
+        UpdateFileStatus();
     }
 }
---
+++ Assets/Editor/LocalCollaborateUserData.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class LocalCollaborateUserData : ScriptableObject {
+    [SerializeField]
+    string userName;
+    [SerializeField]
+    string eMailAddress;
+
+    static LocalCollaborateUserData Instance
+    {
+        get
+        {
+            var Instance = AssetDatabase.LoadAssetAtPath<LocalCollaborateUserData>("Assets/Editor/LocalCollaborateUserData.asset");
+
+            if (Instance == null)
+            {
+                Instance = CreateInstance<LocalCollaborateUserData>();
+                AssetDatabase.CreateAsset(Instance, "Assets/Editor/LocalCollaborateUserData.asset");
+            }
+
+            return Instance;
+        }
+    }
+
+    public static string UserName
+    {
+        get
+        {
+            return Instance.userName;
+        }
+        set
+        {
+            Instance.userName = value;
+        }
+    }
+
+    public static string EMailAddress
+    {
+        get
+        {
+            return Instance.eMailAddress;
+        }
+        set
+        {
+            Instance.eMailAddress = value;
+        }
+    }
+
+    public static LibGit2Sharp.Signature GetSignature()
+    {
+        return new LibGit2Sharp.Signature(UserName, EMailAddress, System.DateTime.Now);
+    }
+}
